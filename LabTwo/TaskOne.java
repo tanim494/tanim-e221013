a5157803c8302a722474d7aba6eddcca399f14fe
Ahsanul Karim Tanim <tanim494@gmail.com>
2026-06-15 11:00:58 +0600

    Create TaskOne.java

---
+++ LabTwo/TaskOne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int sum = 0;
+
+        while(number > 0) {
+            sum += number % 10;
+            number /= 10; 
+        }
+
+        System.out.print("Sum: " + sum);
+    }
+}
